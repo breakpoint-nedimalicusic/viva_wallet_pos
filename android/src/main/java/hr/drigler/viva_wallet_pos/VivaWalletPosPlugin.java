015ca596ad7ef6c144f39ab42d013c94a7ee9f43
breakpoint-nedimalicusic <156081023+breakpoint-nedimalicusic@users.noreply.github.com>
2025-03-18 14:53:14 +0100

    Update VivaWalletPosPlugin.java

--- android/src/main/java/hr/drigler/viva_wallet_pos/VivaWalletPosPlugin.java
+++ android/src/main/java/hr/drigler/viva_wallet_pos/VivaWalletPosPlugin.java
@@ -25,7 +25,6 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class VivaWalletPosPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
 
